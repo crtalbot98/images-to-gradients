56d2ee3a805c5817245ae4ef023e5e6857ea24de
Chandler Talbot <crtalbot98@gmail.com>
2020-09-04 21:53:46 -0400

    fix canvas resizing and getImageData empty error

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -7,8 +7,8 @@ let Canvas = React.memo(function(prop){
     const imgState = React.useContext(ImageContext);
     const [prevImg, setPrevImg] = React.useState(imgState.image);
     const [size, setSize] = React.useState({
-        width: '0px',
-        height: '0px'
+        width: 0,
+        height: 0
     });
 
     const clearImage = (c, ctx) => {
@@ -20,37 +20,40 @@ let Canvas = React.memo(function(prop){
         ctx.putImageData(img, 0, 0);
     };
 
-    const getImageData = (ctx) => {
+    const getImageData = (ctx, size) => {
         let data = {};
         let j = 0;
         const max = Math.max(size.width, size.height);
         const min = Math.min(size.width, size.height);
+        console.log(max, min);
 
         for(let i = 0; i <= max; i += 15){
-            if(i <= min){
-                j++;
-            }
+            if(j >= min) break;
+            if(i <= min) j += 15;
+            console.log(i, j);
             data[i] = ctx.getImageData(i, j,1,1).data;
         }
         console.log(data);
         return data;
     };
 
+    const updateCanvas = async (ctx) => {
+        console.log(imgState.image.width);
+        await setSize({width: imgState.image.width, height: imgState.image.height});
+        await clearImage(prevImg, ctx);
+        await ctx.drawImage(imgState.image, 0,0);
+        await getImageData(ctx, {width: imgState.image.width, height: imgState.image.height});
+    };
+
     React.useEffect(() => {
         if(imgState.image === null) return;
         const ctx = canvas.current.getContext('2d');
-        const updateCanvas = async() => {
-            await setPrevImg(imgState.image);
-            await setSize({width: imgState.image.width, height: imgState.image.height});
-            await clearImage(prevImg, ctx);
-            await ctx.drawImage(imgState.image, 0,0);
-            await getImageData(ctx);
-        };
-        updateCanvas();
-    }, [imgState]);
+        setPrevImg(imgState.image);
+        updateCanvas(ctx);
+    }, [imgState.image]);
 
     return(
-        <canvas id={'image-canvas'} ref={canvas} height={size.height} width={size.width}
+        <canvas id={'image-canvas'} ref={canvas} height={`${size.height}px`} width={`${size.width}px`}
                 data-img={imgState.image !== null ? imgState.image.src : 'empty'}
                 data-testid={'canvas-image'}
         >
