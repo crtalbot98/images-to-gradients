879f517664a117abdb14e04945885c8351ac50d6
Chandler Talbot <crtalbot98@gmail.com>
2021-01-20 20:26:37 -0500

    Refactored imageHandler

--- src/components/usplash-images/ImageHandler.js
+++ src/components/usplash-images/ImageHandler.js
@@ -4,6 +4,8 @@ import ImageList from "./ImageList";
 import {fetchData} from "../../hooks/Hooks";
 import ImagePageBtns from "./ImagePageBtns";
 import ArrowIcon from "./ArrowIcon";
+import {ImageContext} from "../context/ImageContext";
+import ScrollHandler from "../ScrollHandler";
 
 const clickTypes = {
     'Previous': 1,
@@ -13,52 +15,43 @@ const clickTypes = {
 const ImageHandler = React.memo(function(props){
 
     const ImagesLoading = LoadingHandler(ImageList);
+    const ImagesInView = ScrollHandler(ImagePageBtns, ArrowIcon);
+    const [isLoading, setLoading] = React.useState(false);
     const [images, getList] = React.useState([]);
     const [page, setPage] = React.useState(1);
-    const [scrolling, setScrolling] = React.useState(false);
+    const state = React.useContext(ImageContext);
 
     React.useEffect(() => {
-        if(props.loading && props.load){
+        if(state.imagesLoaded){
+            setLoading(true);
             const data = async () => {
                 const imgs = await fetchData(`https://images-to-gradients-server.herokuapp.com/images?page=${page}`);
                 getList(JSON.parse(imgs));
+                if(imgs) setLoading(false);
             };
             data();
         }
-        props.handleLoad(false);
-    }, [props.loading, page]);
-
-    React.useEffect(() => {
-        window.addEventListener('scroll', scrollListener);
-
-        return () => {
-            window.removeEventListener('scroll', scrollListener);
-        }
-    }, [scrolling]);
-
-    const scrollListener = () => {
-        setScrolling(document.body.getBoundingClientRect().top <= -20)
-    };
+    }, [state.imagesLoaded, page]);
 
     const handleClick = (e) => {
         let pageNum = page;
         if(page !== 1 && clickTypes[e.target.innerText] === 1){
             pageNum -= 1;
             setPage(pageNum);
-            props.handleLoad(true);
+            setLoading(true);
         }
         else if(clickTypes[e.target.innerText] === 2){
             pageNum += 1;
             setPage(pageNum);
-            props.handleLoad(true);
+            setLoading(true);
         }
     };
 
     return(
         <React.Fragment>
-            <ImagesLoading isLoading={props.loading} images={images}/>
-            <div className={props.load ? 'flex-cont fixed-btn' : 'hidden'}>
-                {scrolling ? <ImagePageBtns page={page} handleClick={handleClick}/> : <ArrowIcon/>}
+            <ImagesLoading isLoading={isLoading} images={images}/>
+            <div className={state.imagesLoaded ? 'flex-cont fixed-btn' : 'hidden'}>
+                <ImagesInView page={page} handleClick={handleClick}/>
             </div>
         </React.Fragment>
     )
