94f412ee73b1fbb464f3087d99499dd8aedeae45
Chandler Talbot <crtalbot98@gmail.com>
2021-01-20 20:25:48 -0500

    fixed context resetting and added image loading

--- src/components/context/ImageContext.js
+++ src/components/context/ImageContext.js
@@ -2,18 +2,28 @@ import React from "react";
 
 export const ImageContext = React.createContext({ // Create the context
     image: null,
-    setImage: () => {}
+    imagesLoaded: false,
+    setImage: () => {},
+    setImagesLoaded: () => {}
 });
 
 export const ImageProvider = (props) => {
 
     const setImage = (image) => { // Create function to update the image value
-        setState({...state, image: image})
+        setState({...state, image: image});
+        initState.image = image
+    };
+
+    const setImagesLoaded = (loaded) => {
+        setState({...state, imagesLoaded: loaded});
+        initState.imagesLoaded = loaded
     };
 
     const initState = { // Declare the state with the new values
         image: null,
+        imagesLoaded: false,
         setImage: setImage,
+        setImagesLoaded: setImagesLoaded
     };
 
     const [state, setState] = React.useState(initState); // Updates reacts state
