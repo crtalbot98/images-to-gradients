cae8c7e1af77c716956546515572c35069408a00
Chandler Talbot <crtalbot98@gmail.com>
2021-01-20 20:27:17 -0500

    loading icon function

---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Loader(){
+
+    return(
+        <div className={'fixed full center'}>
+            <div className={'loader'}>
+
+            </div>
+        </div>
+    )
+}
+
+export default Loader;
